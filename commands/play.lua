e78eb195a697fa2a637382105e566df63c85da1a
Youri de Vlam <youridevlam@outlook.com>
2022-06-04 20:59:39 +0200

    playlist

--- commands/play.lua
+++ commands/play.lua
@@ -1,11 +1,54 @@
 local spawn = require('coro-spawn')
+local split = require('coro-split')
 local parse = require('url').parse
+local isActive = false
+local songQueue = {}
 
+function getStream(url)
+    local child = spawn('youtube-dl', {
+        args = {'-g', url},
+        stdio = {nil, true, true}
+    })
+    local stream
+    function readstdout()
+        local stdout = child.stdout
+        for chunk in stdout.read do
+            local mime = parse(chunk, true).query.mime
+            if mime and mime:find('audio') then
+                stream = chunk
+            end
+        end
+        return pcall(stdout.handle.close, stdout.handle)
+    end
+    function readstderr()
+        local stderr = child.stderr
+        for chunk in stderr.read do
+            print(chunk)
+        end
+        return pcall(stderr.handle.close, stderr.handle)
+    end
+    split(readstdout, readstderr, child.waitExit)
+    return stream and stream:gsub('%c', '')
+end
+
+local play
+play = function(vc, connection)
+    if next(songQueue) == nil then
+        connection:close()
+        isActive = false
+        return
+    end
+    local conn = vc:join()
+    isActive = true
+    conn:playFFmpeg(table.remove(songQueue,1))
+    play(vc, conn)
+end
 
 return {
 	name = 'play',
 	description = 'description',
     command = function(args, message, client, rest)
+
         local requester = message.guild:getMember(message.author)
         local vc = requester.voiceChannel
 
@@ -14,26 +57,9 @@ return {
             return
         end
 
-        local connection = vc:join()
-        local child = spawn('youtube-dl', {
-            args = {'-g', args[1]},
-            stdio = { nil, true, 2 }
-        })
-
-        local stream
-        for chunk in child.stdout.read do
-            local urls = chunk:split('\n')
-
-            for _, yturl in pairs(urls) do
-                local mime = parse(yturl, true).query.mime
-
-                if mime and mime:find('audio') == 1 then
-                    stream = yturl
-                end
-            end
+        table.insert(songQueue, getStream(args[1]))
+        if not isActive then
+            play(vc, nil)
         end
-        message.channel:send('Playing '..args[1]..' :musical_note:')
-        connection:playFFmpeg(stream)
-        connection:close()
 	end
 };
